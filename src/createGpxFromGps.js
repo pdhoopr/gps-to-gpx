aad6c50ce86cfcf2351b3f4db5de23afdf0299f1
Patrick Hooper <patrick.d.hooper@gmail.com>
2016-06-27 10:53:40 -0400

    Write tests and code for argument validation

--- src/createGpxFromGps.js
+++ src/createGpxFromGps.js
@@ -1,3 +1,45 @@
-export default function createGpxFromGps() {
+import xmlBuilder from 'xmlbuilder';
+
+function doesArgExist(argValue) {
+  return argValue !== undefined && argValue !== null;
+}
+
+function isArgCorrectType(argValue, type) {
+  const argTypeMatch = Object.prototype.toString.call(argValue).match(/\[object (.+)\]/i);
+  const argType = argTypeMatch.length ? argTypeMatch[1] : 'undefined';
+
+  return argType === type;
+}
+
+function assertArgsAreValid(activity, time, data) {
+  const invalidArgs = [];
+
+  if (!doesArgExist(activity) || !isArgCorrectType(activity, 'String')) {
+    invalidArgs.push('activity');
+  }
+
+  if (!doesArgExist(time) || !isArgCorrectType(time, 'String')) {
+    invalidArgs.push('time');
+  }
+
+  if (!doesArgExist(data) || !isArgCorrectType(data, 'Array') || !data.length) {
+    invalidArgs.push('data');
+  }
+
+  if (invalidArgs.length) {
+    throw new Error(
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: ${invalidArgs.join(', ')}. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  }
+
+  return;
+}
+
+export default function createGpxFromGps(activity, time, data) {
+  assertArgsAreValid(activity, time, data);
+
   return;
 }
--- test/createGpxFromGps.test.js
+++ test/createGpxFromGps.test.js
@@ -2,4 +2,102 @@ import { expect } from 'chai';
 import { createGpxFromGps } from '../src';
 
 describe('createGpxFromGps', () => {
+  it('should throw an error if no parameters are provided (i.e. undefined)', () => {
+    expect(() => {
+      createGpxFromGps();
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: activity, time, data. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if provided parameters are null', () => {
+    expect(() => {
+      createGpxFromGps(null, null, null);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: activity, time, data. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "activity" parameter is not provided', () => {
+    expect(() => {
+      createGpxFromGps(null, 'startTime', [{}]);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: activity. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "time" parameter is not provided', () => {
+    expect(() => {
+      createGpxFromGps('activity', null, [{}]);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: time. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "data" parameter is not provided', () => {
+    expect(() => {
+      createGpxFromGps('activity', 'time', null);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: data. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "activity" parameter is of a type other than "string"', () => {
+    expect(() => {
+      createGpxFromGps(1, 'time', [{}]);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: activity. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "time" parameter is of a type other than "string"', () => {
+    expect(() => {
+      createGpxFromGps('activity', true, [{}]);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: time. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "data" parameter is of a type other than "array"', () => {
+    expect(() => {
+      createGpxFromGps('activity', 'time', {});
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: data. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
+
+  it('should throw an error if "data" parameter is empty (has no length)', () => {
+    expect(() => {
+      createGpxFromGps('activity', 'time', []);
+    }).to.throw(Error,
+      `createGpxFromGps expected the parameters (activity, time, data) to exist and be of the ` +
+      `correct type, but the following were invalid: data. Did you pass ` +
+      `arguments that satisfy the order and types (activity: String, time: String, data: Array) ` +
+      `when you called the function?`
+    );
+  });
 });
