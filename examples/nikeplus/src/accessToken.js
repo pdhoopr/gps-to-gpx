30df403a460d2418691bed3c9125927d6390a1e6
Patrick Hooper <patrick.d.hooper@gmail.com>
2016-08-13 17:55:25 -0400

    Remove curly braces around NIKE+_API_ACCESS_TOKEN_GOES_HERE

--- examples/nikeplus/src/accessToken.js
+++ examples/nikeplus/src/accessToken.js
@@ -1,7 +1,7 @@
-// Replace `{NIKE+_API_ACCESS_TOKEN_GOES_HERE}` with your Nike+ API access token. I get mine by
+// Replace `NIKE+_API_ACCESS_TOKEN_GOES_HERE` with your Nike+ API access token. I get mine by
 // going to `https://developer.nike.com/documentation/api-docs/test-console.html`, logging in, and
 // then grabbing the `access_token` param sent in test console requests. There's probably a better
 // way to do this...
-const ACCESS_TOKEN = '{NIKE+_API_ACCESS_TOKEN_GOES_HERE}';
+const ACCESS_TOKEN = 'NIKE+_API_ACCESS_TOKEN_GOES_HERE';
 
 export default ACCESS_TOKEN;
